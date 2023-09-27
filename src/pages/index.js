f448dcb7ece4981b2bfc99d81b0468aabf0070c3
Flash13076 <pietesejeandre@gmail.com>
2023-09-27 14:58:28 +0200

    styling

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,6 +10,7 @@ import {
   MenuItem,
   Select,
 } from "@mui/material";
+import Head from "next/head";
 
 const Calculator = () => {
   const [input1, setInput1] = useState("");
@@ -61,171 +62,179 @@ const Calculator = () => {
   };
 
   return (
-    <Box
-      display="flex"
-      flexDirection="column"
-      alignItems="center"
-      maxWidth="400px"
-      mx="auto"
-      border="1px solid #ccc"
-      borderRadius="8px"
-      boxShadow="0 2px 4px rgba(0, 0, 0, 0.1)"
-      sx={{ py: 4, px: 4 }}
-    >
-      <Typography variant="h5" gutterBottom>
-        Compound Interest Calculator
-      </Typography>
-      <TextField
-        fullWidth
-        label="Amount"
-        variant="outlined"
-        type="number"
-        value={input1}
-        InputProps={{
-          startAdornment: <InputAdornment position="start">R</InputAdornment>,
-        }}
-        onChange={(e) => setInput1(e.target.value)}
-        margin="normal"
-      />
-      <TextField
-        fullWidth
-        label="Interest rate"
-        variant="outlined"
-        type="number"
-        value={input2}
-        InputProps={{
-          endAdornment: <InputAdornment position="start">%</InputAdornment>,
-        }}
-        onChange={(e) => setInput2(e.target.value)}
-        margin="normal"
-      />
-      <TextField
-        fullWidth
-        label="Duration of investement(Y)"
-        variant="outlined"
-        type="number"
-        value={input3}
-        onChange={(e) => setInput3(e.target.value)}
-        margin="normal"
-      />
-      <FormControl fullWidth variant="outlined" style={{ marginTop: "16px" }}>
-        <InputLabel id="demo-simple-select-outlined-label">
-          Interval of investment payment
-        </InputLabel>
-        <Select
-          label="Interval of investment payment"
-          value={input4}
-          onChange={(e) => setInput4(e.target.value)}
-        >
-          <MenuItem value={12}>Monthly</MenuItem>
-          <MenuItem value={4}>Quarterly</MenuItem>
-          <MenuItem value={2}>Bi-annually</MenuItem>
-          <MenuItem value={1}>Annually</MenuItem>
-        </Select>
-      </FormControl>
+    <>
+      <Head>
+        <title>Financial Calculator</title>
+        <meta name="description" content="Financial Calculator" />
+        <link rel="icon" href="/head.png" />
+      </Head>
+
       <Box
-        sx={{
-          display: "flex",
-          flexDirection: { sm: "row", xs: "column" },
-          alignItems: "center",
-          justifyContent: "space-between",
-          width: "80%",
-          gap: 1,
-        }}
+        display="flex"
+        flexDirection="column"
+        alignItems="center"
+        maxWidth="400px"
+        mx="auto"
+        border="1px solid #ccc"
+        borderRadius="8px"
+        boxShadow="0 2px 4px rgba(0, 0, 0, 0.1)"
+        sx={{ py: 4, px: 4 }}
       >
+        <Typography variant="h5" gutterBottom>
+          Compound Interest Calculator
+        </Typography>
+        <TextField
+          fullWidth
+          label="Amount"
+          variant="outlined"
+          type="number"
+          value={input1}
+          InputProps={{
+            startAdornment: <InputAdornment position="start">R</InputAdornment>,
+          }}
+          onChange={(e) => setInput1(e.target.value)}
+          margin="normal"
+        />
+        <TextField
+          fullWidth
+          label="Interest rate"
+          variant="outlined"
+          type="number"
+          value={input2}
+          InputProps={{
+            endAdornment: <InputAdornment position="start">%</InputAdornment>,
+          }}
+          onChange={(e) => setInput2(e.target.value)}
+          margin="normal"
+        />
+        <TextField
+          fullWidth
+          label="Duration of investement(Y)"
+          variant="outlined"
+          type="number"
+          value={input3}
+          onChange={(e) => setInput3(e.target.value)}
+          margin="normal"
+        />
+        <FormControl fullWidth variant="outlined" style={{ marginTop: "16px" }}>
+          <InputLabel id="demo-simple-select-outlined-label">
+            Interval of investment payment
+          </InputLabel>
+          <Select
+            label="Interval of investment payment"
+            value={input4}
+            onChange={(e) => setInput4(e.target.value)}
+          >
+            <MenuItem value={12}>Monthly</MenuItem>
+            <MenuItem value={4}>Quarterly</MenuItem>
+            <MenuItem value={2}>Bi-annually</MenuItem>
+            <MenuItem value={1}>Annually</MenuItem>
+          </Select>
+        </FormControl>
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: { sm: "row", xs: "column" },
+            alignItems: "center",
+            justifyContent: "space-between",
+            width: "80%",
+            gap: 1,
+          }}
+        >
+          <Button
+            variant="contained"
+            color="primary"
+            onClick={calculateCompoundInterest}
+            style={{ marginTop: "16px" }}
+          >
+            Compound interest
+          </Button>
+
+          <Button
+            variant="contained"
+            color="primary"
+            onClick={calculateSimpleInterest}
+            style={{ marginTop: "16px" }}
+          >
+            Simple interest
+          </Button>
+
+          <Button
+            variant="contained"
+            color="primary"
+            onClick={calculateFutureValue}
+            style={{ marginTop: "16px" }}
+          >
+            Future value
+          </Button>
+        </Box>
+
+        <Typography variant="h6" style={{ marginTop: "16px" }}>
+          Final amount: {output}
+        </Typography>
+
+        <p></p>
+        <p></p>
+        <Typography variant="h5" gutterBottom>
+          Retirement Fund Calculator
+        </Typography>
+        <TextField
+          fullWidth
+          label="Desired monthly retirement income"
+          variant="outlined"
+          type="number"
+          value={input5}
+          InputProps={{
+            startAdornment: <InputAdornment position="start">R</InputAdornment>,
+          }}
+          onChange={(e) => setInput5(e.target.value)}
+          margin="normal"
+        />
+        <TextField
+          fullWidth
+          label="Interest rate"
+          variant="outlined"
+          type="number"
+          value={input6}
+          InputProps={{
+            endAdornment: <InputAdornment position="start">%</InputAdornment>,
+          }}
+          onChange={(e) => setInput6(e.target.value)}
+          margin="normal"
+        />
+        <TextField
+          fullWidth
+          label="Years till retirement"
+          variant="outlined"
+          type="number"
+          value={input7}
+          onChange={(e) => setInput7(e.target.value)}
+          margin="normal"
+        />
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: { sm: "row", xs: "column" },
+            alignItems: "center",
+            justifyContent: "space-between",
+            width: "80%",
+            gap: 1,
+          }}
+        ></Box>
         <Button
           variant="contained"
           color="primary"
-          onClick={calculateCompoundInterest}
+          onClick={calculateRetirementFund}
           style={{ marginTop: "16px" }}
         >
-          Compound interest
+          Calculate monthly investment
         </Button>
 
-        <Button
-          variant="contained"
-          color="primary"
-          onClick={calculateSimpleInterest}
-          style={{ marginTop: "16px" }}
-        >
-          Simple interest
-        </Button>
-
-        <Button
-          variant="contained"
-          color="primary"
-          onClick={calculateFutureValue}
-          style={{ marginTop: "16px" }}
-        >
-          Future value
-        </Button>
+        <Typography variant="h6" style={{ marginTop: "16px" }}>
+          Necessary investement: {outputRet}
+        </Typography>
       </Box>
-
-      <Typography variant="h6" style={{ marginTop: "16px" }}>
-        Final amount: {output}
-      </Typography>
-
-      <p></p>
-      <p></p>
-      <Typography variant="h5" gutterBottom>
-        Retirement Fund Calculator
-      </Typography>
-      <TextField
-        fullWidth
-        label="Desired monthly retirement income"
-        variant="outlined"
-        type="number"
-        value={input5}
-        InputProps={{
-          startAdornment: <InputAdornment position="start">R</InputAdornment>,
-        }}
-        onChange={(e) => setInput5(e.target.value)}
-        margin="normal"
-      />
-      <TextField
-        fullWidth
-        label="Interest rate"
-        variant="outlined"
-        type="number"
-        value={input6}
-        InputProps={{
-          endAdornment: <InputAdornment position="start">%</InputAdornment>,
-        }}
-        onChange={(e) => setInput6(e.target.value)}
-        margin="normal"
-      />
-      <TextField
-        fullWidth
-        label="Years till retirement"
-        variant="outlined"
-        type="number"
-        value={input7}
-        onChange={(e) => setInput7(e.target.value)}
-        margin="normal"
-      />
-      <Box
-        sx={{
-          display: "flex",
-          flexDirection: { sm: "row", xs: "column" },
-          alignItems: "center",
-          justifyContent: "space-between",
-          width: "80%",
-          gap: 1,
-        }}
-      ></Box>
-      <Button
-        variant="contained"
-        color="primary"
-        onClick={calculateRetirementFund}
-        style={{ marginTop: "16px" }}
-      >
-        Calculate monthly investment
-      </Button>
-
-      <Typography variant="h6" style={{ marginTop: "16px" }}>
-        Necessary investement: {outputRet}
-      </Typography>
-    </Box>
+    </>
   );
 };
 
